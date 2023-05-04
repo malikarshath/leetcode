da04e8cf60d43f5f7f039ff06ef422c4fb4466ed
arsha <97086801+malikarshath@users.noreply.github.com>
2023-05-04 13:21:11 +0530

    Added solution - LeetHub

---
+++ Delete node in Doubly Linked List - GFG/delete-node-in-doubly-linked-list.py
@@ -0,0 +1,84 @@
+#User function Template for python3
+
+'''class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+'''
+class Solution:
+    def deleteNode(self,head, x):
+        # Code here
+        current = head
+        for i in range(x):
+            if(i == x-1):
+                if current.prev == None:
+                    head  = current.next 
+                    current.next.prev = None
+                    current = None
+                    return head
+                elif current.next == None:
+                    current.prev.next = None
+                    current = None
+                    return head
+                else:
+                    current.next.prev = current.prev
+                    current.prev.next = current.next
+                    return head
+            current = current.next
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = None
+        if self.head is None:
+            new_node.prev = None
+            self.head = new_node
+            self.tail = self.head
+            return
+        self.tail.next = new_node
+        self.tail.next.prev = self.tail
+        self.tail = self.tail.next
+
+    def printList(self, node):
+        while (node.next is not None):
+            node = node.next
+        while node.prev is not None:
+            node = node.prev
+        while (node is not None):
+            print(node.data, end=" ")
+            node = node.next
+        print()
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        llist = DoublyLinkedList()
+        for e in arr:
+            llist.append(e)
+        pos = int(input())
+        Solution().deleteNode(llist.head, pos)
+        llist.printList(llist.head)
+# Contributed by: Harshit Sidhwa
+
+                               
+# } Driver Code Ends
