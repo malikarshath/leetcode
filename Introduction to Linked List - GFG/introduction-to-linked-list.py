b4b7c3271edd8e0805b243053ac8781b38005600
arsha <97086801+malikarshath@users.noreply.github.com>
2023-05-04 11:44:26 +0530

    Added solution - LeetHub

---
+++ Introduction to Linked List - GFG/introduction-to-linked-list.py
@@ -0,0 +1,45 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+'''
+# Node Class
+	class Node:
+	    def __init__(self, data):   # data -> value stored in node
+	        self.data = data
+	        self.next = None
+'''
+class Solution:
+    def constructLL(self, arr):
+        # code here
+        for i in range(len(arr)):
+            if i == 0:
+                node = Node(arr[i])
+                head = node
+            else:
+                node.next = Node(arr[i])
+                node = node.next
+        return head 
+
+#{ 
+ # Driver Code Starts.
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        n = int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.constructLL(arr)
+        while res:
+            print(res.data, end = ' ')
+            res = res.next
+        print()
+# } Driver Code Ends
