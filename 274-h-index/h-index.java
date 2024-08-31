0b5b3a8de999f5635a33acbe3da38e7c97e908a6
arsha <97086801+malikarshath@users.noreply.github.com>
2024-08-31 22:28:51 +0530

    Time: 2 ms (72.22%) | Memory: 41.5 MB (53.24%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,41 +1,45 @@
 class Solution {
     public int hIndex(int[] citations) {
+        //Arrays.sort(citations, Collections.reverseOrder());
         Arrays.sort(citations);
+
         int n = citations.length;
+        int l = 0, r = n - 1;
+
+        while(l < r) {
+            int temp = citations[l];
+            citations[l] = citations[r];
+            citations[r] = temp;
+            l++;
+            r--;
+        }
 
         int ans = 0;
 
-        //Map<Integer, Integer> mp = new HashMap<>();
-
-        for(int i = 0; i <= n; i++) {
-            int freq = 0;
-            for(int j = 0; j < n; j++) {
-                if(citations[j] >= i) {
-                    freq = n - j;
-                    break;
-                }
-            }
-            if(freq >= i) {
-                ans = i;
-            }
-            //mp.put(i, freq);
-
-        }
-
-
-        if(n == 1 && citations[0] >= 1) {
-            return 1;
-        }
-
-
-        // for(int i = 0; i < n; i++) {
-
-        //     if(n - i == citations[i]) {
-        //         ans = n - i;
-        //     } else if(n - i > citations[i]) {
-        //         ans = citations[i];
+        // for(int i = 0; i <= n; i++) {
+        //     int freq = 0;
+        //     for(int j = 0; j < n; j++) {
+        //         if(citations[j] >= i) {
+        //             freq = n - j;
+        //             break;
+        //         }
         //     }
+        //     if(freq >= i) {
+        //         ans = i;
+        //     }
+
         // }
+
+        for(int i = 0; i < n; i++) {
+
+            int hIndex = i + 1;
+
+            if(citations[i] >= hIndex) { // 0 1 3 6 5
+                ans = hIndex;
+            }
+        }
+
+    
 
         return ans;
         
