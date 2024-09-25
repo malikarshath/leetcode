de4dbc2522b2c6e5016199eb617ab8eb3f121534
arsha <97086801+malikarshath@users.noreply.github.com>
2024-09-25 20:31:04 +0530

    Time: 2 ms (83.36%) | Memory: 44.8 MB (16.49%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,30 +1,28 @@
 class Solution {
     List<List<Integer>> finalAns;
-    Set<List<Integer>> finalSet;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         finalAns = new ArrayList<>();
-        finalSet = new HashSet<>();
         List<Integer> li = new ArrayList<>();
-        sum(candidates, target, li);
+        Arrays.sort(candidates);
+        sum(candidates, target, li, 0);
         return finalAns;
     }
 
-    public void sum(int[] candidates, int target, List<Integer> li) {
+    public void sum(int[] candidates, int target, List<Integer> li, int start) {
         if(target == 0) {
             List<Integer> ans = new ArrayList<>(li);
-            Collections.sort(ans);
-            if(!finalSet.contains(ans)) {
-                finalAns.add(ans);
-                finalSet.add(ans);
-            }
+            finalAns.add(ans);
             return;
         }
         if(target < 0) {
             return;
         }
-        for(int can : candidates) {
-            li.add(can);
-            sum(candidates, target - can, li);
+        for(int i = start; i < candidates.length; i++) {
+            if(target - candidates[i] < 0) {
+                break;
+            }
+            li.add(candidates[i]);
+            sum(candidates, target - candidates[i], li, i);
             li.remove(li.size() - 1);
         }
     }
