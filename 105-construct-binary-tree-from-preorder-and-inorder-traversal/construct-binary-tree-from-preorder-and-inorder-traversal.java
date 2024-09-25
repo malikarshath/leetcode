583c3feb271c4ebcfb1a35ac0aea145bd01ea70a
arsha <97086801+malikarshath@users.noreply.github.com>
2024-09-25 17:30:28 +0530

    Time: 2 ms (68.62%) | Memory: 44.4 MB (43.60%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -19,31 +19,37 @@ class Solution {
         if(preorder.length != inorder.length) {
             return null;
         }
-        return construct(0, n - 1, 0, n - 1, preorder, inorder);
+
+        Map<Integer, Integer> mp = new HashMap();
+
+        for(int i = 0; i < n; i++) {
+            mp.put(inorder[i], i);
+        }
+        return construct(0, n - 1, 0, n - 1, preorder, inorder, mp);
     }
 
-    public TreeNode construct(int ps, int pe, int is, int ie, int[] preorder, int[] inorder) {
+    public TreeNode construct(int ps, int pe, int is, int ie, int[] preorder, int[] inorder,Map<Integer, Integer> mp) {
         if(ps > pe || is > ie) {
             return null;
         }
-        int rootIndex = -1;
 
         TreeNode root = new TreeNode(preorder[ps]);
+        int rootIndex = mp.containsKey(root.val) ? mp.get(root.val) : -1;
 
-        for(int i = is ; i <= ie; i++) {
-            if(inorder[i] == root.val) {
-                rootIndex = i;
-                break;
-            }
-        } 
+        // for(int i = is ; i <= ie; i++) {
+        //     if(inorder[i] == root.val) {
+        //         rootIndex = i;
+        //         break;
+        //     }
+        // } 
 
         if(rootIndex == -1) {
             return null;
         }
         int len = rootIndex - is;
 
-        root.left = construct(ps + 1, ps + len, is, rootIndex - 1, preorder, inorder);
-        root.right = construct(ps + len + 1, pe, rootIndex + 1, ie, preorder, inorder);
+        root.left = construct(ps + 1, ps + len, is, rootIndex - 1, preorder, inorder, mp);
+        root.right = construct(ps + len + 1, pe, rootIndex + 1, ie, preorder, inorder, mp);
         return root;
     }
 }
