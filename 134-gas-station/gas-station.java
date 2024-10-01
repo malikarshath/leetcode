13ea70ee0cfe807e78dd2190c2070fb2954b4faa
arsha <97086801+malikarshath@users.noreply.github.com>
2024-10-01 22:01:30 +0530

    Time: 2 ms (97.81%) | Memory: 55.9 MB (82.68%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,32 +1,24 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
 
-        // approach -- sum of gas needs to be greater than the sum of cost if we start from the  particular index.
+        // approach -- sum of gas needs to be greater than the sum of cost if we start from the  
+        // particular index.
         int n = gas.length;
-        int idx = -1;
-
-        int totalGas = 0;
+        int idx = 0;
         int totalCost = 0;
-        int currentGas = 0;
-        int currentCost = 0;
+        int totalGas = 0;
+
+        int remainingGas = 0;
 
         for(int i = 0; i < n; i++) {
+            remainingGas += gas[i]- cost[i];
             totalGas += gas[i];
             totalCost += cost[i];
-            if(idx != -1) {
-                currentGas += gas[i];
-                currentCost += cost[i];
-            }
 
-            if(currentGas < currentCost) { // if totalGas is less then totalCost than till current index gas is not enough to travel
-                idx = -1;
-            }
-
-            if(gas[i] >= cost[i] && idx == -1) { // if there tank is empty that means idx = -1 then we will consider the current index as the starting point if gas >= cost
-                idx = i;
-                currentGas = gas[i];
-                currentCost = cost[i];
-            }
+            if(remainingGas < 0) {
+                remainingGas = 0;
+                idx = i + 1;
+            }   
 
         }
         if(totalCost > totalGas) return -1; // gas is not enough for travel 
