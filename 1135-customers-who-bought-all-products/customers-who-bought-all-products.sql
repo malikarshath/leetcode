2d26814203174c91ebf058d58e5bf338057ead80
arsha <97086801+malikarshath@users.noreply.github.com>
2024-10-13 17:31:23 +0530

    Time: 497 ms (83.50%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -2,7 +2,5 @@
 
 SELECT C.customer_id
 FROM Customer C
-JOIN Product P
-ON C.product_key = P.product_key
 GROUP BY C.customer_id
 HAVING COUNT(DISTINCT(C.product_key)) = (SELECT COUNT(*) FROM Product);
