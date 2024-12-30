f9ab26e8d1004016e4fb3836c89731e9f4091424
arsha <97086801+malikarshath@users.noreply.github.com>
2024-12-30 10:51:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (8.52%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,44 +1,37 @@
 class Solution {
     public int search(int[] nums, int target) {
-
-        int n = nums.length;
         int s = 0;
-        int e = n - 1;
-
-        int ans = -1;
-
+        int e = nums.length - 1;
+        
         while(s <= e) {
-            int m = ((e - s) / 2) + s;
-
-            if(nums[m] == target) {
-                ans = m;
-                break;
+            int m = (e - s)/ 2 + s;
+            if(nums[m] ==  target) {
+                return m;
             }
-
-            if(target >= nums[0]) { // t in 1
-                if(nums[m] < nums[0]) { // m in 2
-                    e = m - 1;
-                } else { // m in 1
-                    if(nums[m] > target) {
-                        e = m - 1;
-                    } else if(nums[m] < target) {
+            if(target < nums[0]) { // target part 2
+                if(nums[m] >= nums[0]) { // mid in part 1
+                    s = m + 1;
+                } else { // mid and target in part 2
+                    if(target >= nums[m]) {
                         s = m + 1;
+                    } else {
+                        e = m - 1;
                     }
                 }
-            } else { // t in 2
-                if(nums[m] >= nums[0]) { // m in 1
-                    s = m + 1;
-                } else { // m in 2
-                    if(nums[m] > target) {
+            } else { // target part 1
+                if(nums[m] < nums[0]) { // mid in part 2
+                    e = m - 1;
+                } else { // mid and target in part 1
+                    if(nums[m] >= target) {
                         e = m - 1;
-                    } else if(nums[m] < target) {
+                    } else {
                         s = m + 1;
                     }
                 }
             }
         }
 
-        return ans;
-        
+        return -1;
+
     }
 }
