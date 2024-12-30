e302142f6b9bcc67a8362e191cff487b9759429b
arsha <97086801+malikarshath@users.noreply.github.com>
2024-12-30 10:26:09 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (48.19%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+
+        while(s <= e) {
+            int m = (e - s)/2 + s;
+            if(nums[m] == target) {
+                return m;
+            } else if(nums[m] > target) {
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+
+        return -1;
+    }
+}
